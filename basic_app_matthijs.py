433bc214991ff2848b9f4452f92caaef156ec43c
Nicknameisnick <wessel.arend@gmail.com>
2025-09-17 17:13:25 +0200

    Update basic_app_matthijs.py

--- basic_app_matthijs.py
+++ basic_app_matthijs.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 import numpy as np
-import seaborn as sns
 
 pd.options.display.max_rows = 999
 pd.set_option('display.width', 1000)       
@@ -74,5 +73,6 @@ fig = px.line(
 fig.update_traces(mode='lines+markers')
 
 fig.show()
+
 
 
